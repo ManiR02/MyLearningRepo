5d3ffae14b1b4811a5b200f6c86e2ae86982d252
ManiR02 <mani.cse02@gmail.com>
2017-07-12 00:45:53 +0530

    POM
    Page Object Model

---
+++ src/main/java/pages/DuplicateLeadPage.java
@@ -0,0 +1,45 @@
+package pages;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.relevantcodes.extentreports.ExtentTest;
+
+import wrappers.LeafTapsWrappers;
+
+public class DuplicateLeadPage extends LeafTapsWrappers{
+
+	public DuplicateLeadPage(RemoteWebDriver driver, ExtentTest test){
+		this.driver = driver;
+		this.test = test;
+
+		if(!verifyTitle("Duplicate Lead | opentaps CRM")){
+			reportStep("This is not Duplicate Lead Page", "FAIL");
+		}
+
+	}
+
+	public DuplicateLeadPage verifyDuplicateLeadHeaderTitle(){
+		verifyTextByXpath("//div[contains(text(),'Duplicate Lead')]", "Duplicate Lead");
+		return this;
+	}
+
+	public ViewLeadPage clickCreateLeadButton(){
+		clickByClassName("smallSubmit");
+		return new ViewLeadPage(driver, test);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
