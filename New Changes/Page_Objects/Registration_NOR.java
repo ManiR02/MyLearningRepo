397277649aa0e6800a2c048d1910b4ff23de4953
ManiR02 <mani.cse02@gmail.com>
2017-09-13 19:19:33 +0530

    New changes added
    commit

--- New Changes/Page_Objects/Registration_NOR.java
+++ New Changes/Page_Objects/Registration_NOR.java
@@ -1,6 +1,20 @@
 package Page_Objects;
 
+import java.io.File;
 import java.util.Hashtable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import utils.ReadExcel;
 
 import wrappers.FunctionLibrary;
 
@@ -8,6 +22,20 @@ public class Registration_NOR extends FunctionLibrary {
 
 	public synchronized String Registration_Page(String locator){
 
+		String multiLanguageExcelPath = System.getProperty("user.dir")+property.getProperty("resourceMgmtfilekeyPath");
+
+		String languageName = property.getProperty("Language_Required");
+		
+		ReadExcel excel=new ReadExcel(multiLanguageExcelPath);
+		
+		String[] xpathsplittext = null;
+		String resourceFileName = null;
+		String filePathLocation = null;
+		NodeList nodeList=null;
+		String Resxfilevalue = null;
+		String[] SplitResxfilevalue = null;
+		String finalresxfilevalue = null;
+		
 		try{
 			Hashtable<String, String> hs = new Hashtable<String, String>();
 			//Murali -start
@@ -17,7 +45,7 @@ public class Registration_NOR extends FunctionLibrary {
 			hs.put("btnNewRegister", "id#btnNewRegister");
 			hs.put("portoutalert", "id#spnPortinSubscriberMsg");
 			hs.put("btnFindAddress", "xpath#.//*[@id='btnFindAddress']/span");
-			hs.put("lblPersonalInfo", "xpath#//div[text()='Personal Information']");
+			hs.put("lblPersonalInfo", "xpath#//div[text()='RegistrationResources>>PersonalInformation']");
 			hs.put("restrictErrorMsg", "id#restrictErrorMsg");
 			hs.put("txtEmail1", "id#txtEmail1");
 			hs.put("EmailErrorMsg", "id#txtEmail1-error");
@@ -83,21 +111,21 @@ public class Registration_NOR extends FunctionLibrary {
 			hs.put("Submit_button", "id#btnNORSubmit");
 			hs.put("SuccessLabel", "id#step1error");
 			//view registration
-			hs.put("lblview_Title", "xpath#.//label[text()='Title']/ancestor::div/div[@class='form-group col-md-1']");
-			hs.put("lblview_FirstName", "xpath#.//label[text()='Title']/ancestor::div/div[@class='form-group col-md-2'][1]");
-			hs.put("lblview_Account", "xpath#.//label[contains(text(),'Account Number')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_LastName", "xpath#.//label[text()='Title']/ancestor::div/div[@class='form-group col-md-2'][2]");
-			hs.put("lblview_DOB", "xpath#.//label[contains(text(),'Date Of Birth')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_Nationality", "xpath#.//label[contains(text(),'Nationality')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_MSISDN", "xpath#.//label[contains(text(),'MSISDN')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_IMSI1", "xpath#.//label[contains(text(),'IMSI-1')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_IMSI2", "xpath#.//label[contains(text(),'IMSI-1')]/parent::div/following-sibling::div[3]");
-			hs.put("lblview_SIMNumber", "xpath#.//label[contains(text(),'MSISDN')]/parent::div/following-sibling::div[3]");
-			hs.put("lblview_PUKCode", "xpath#.//label[contains(text(),'PUK code')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_Email", "xpath#.//label[contains(text(),'Email ID')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_Language", "xpath#.//label[contains(text(),'Language')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_Contact", "xpath#.//label[contains(text(),'Contact')]/parent::div/following-sibling::div[1]");
-			hs.put("lblview_SMSMarketing", "xpath#.//label[contains(text(),'SMS')]");
+			hs.put("lblview_Title", "xpath#.//label[text()='RegistrationResources>>Title']/ancestor::div/div[@class='form-group col-md-1']");
+			hs.put("lblview_FirstName", "xpath#.//label[text()='RegistrationResources>>Title']/ancestor::div/div[@class='form-group col-md-2'][1]");
+			hs.put("lblview_Account", "xpath#.//label[contains(text(),'RegistrationResources>>AccountNo')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_LastName", "xpath#.//label[text()='RegistrationResources>>Title']/ancestor::div/div[@class='form-group col-md-2'][2]");
+			hs.put("lblview_DOB", "xpath#.//label[contains(text(),'RegistrationResources>>DateOfBirth')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_Nationality", "xpath#.//label[contains(text(),'RegistrationResources>>Nationality')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_MSISDN", "xpath#.//label[contains(text(),'RegistrationResources>>MSISDN')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_IMSI1", "xpath#.//label[contains(text(),'RegistrationResources>>IMSI1')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_IMSI2", "xpath#.//label[contains(text(),'RegistrationResources>>IMSI1')]/parent::div/following-sibling::div[3]");
+			hs.put("lblview_SIMNumber", "xpath#.//label[contains(text(),'RegistrationResources>>MSISDN')]/parent::div/following-sibling::div[3]");
+			hs.put("lblview_PUKCode", "xpath#.//label[contains(text(),'RegistrationResources>>PUKNumber')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_Email", "xpath#.//label[contains(text(),'RegistrationResources>>EmailID')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_Language", "xpath#.//label[contains(text(),'RegistrationResources>>PreferredLanguage')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_Contact", "xpath#.//label[contains(text(),'RegistrationResources>>Contact')]/parent::div/following-sibling::div[1]");
+			hs.put("lblview_SMSMarketing", "xpath#.//label[contains(text(),'RegistrationResources>>SMS')]");
 			hs.put("btnview_Cancel", "xpath#//button[@id='refreshSubscriber']");
 			hs.put("lblview_Country", "xpath#.//*[@id='psteps_circle_steps_simple']/div/div/form/div/div[7]/div");
 			hs.put("lblview_state", "xpath#.//*[@id='psteps_circle_steps_simple']/div/div/form/div/div[8]/div[1]");
@@ -108,8 +136,8 @@ public class Registration_NOR extends FunctionLibrary {
 			hs.put("btnReset", "id#submitResetPassword");
 			hs.put("resetPassMsg","id#resetPassMsg");
 			hs.put("rotateElement","xpath#.//*[@id='resetPassMsg'][@class='glyphicon-rotate']");
-			hs.put("view_reg_CloseButton", "xpath#.//button[contains(text(),'×')]");
-			hs.put("viewSubReg_CloseBtn", "xpath#//li[@class='close-icon']");
+			hs.put("view_reg_CloseButton", "xpath#.//button[contains(text(),'Ã—')]");
+			hs.put("viewSubReg_CloseBtn", "xpath#//div[@class='close-icon close-view-icon']");
 			hs.put("edit_RegBtn", "id#editPreRegxn");
 			hs.put("edit_CancelBtn", "id#Cancel");
 			//added by murali on 30-09-2016		
@@ -126,7 +154,122 @@ public class Registration_NOR extends FunctionLibrary {
 			hs.put("Uploaded_File", "xpath#//a[@class='FloatLeft']");
 			hs.put("Confirm_Uploaded_File", "id#lblAttPathText");
 			hs.put("Load_Address_Image", "id#btnNorrotate1");
-			return hs.get(locator);
+			
+			String xpathValue = hs.get(locator);
+			
+			if(xpathValue.contains("text()=")){
+			
+				System.out.println("text()");	
+					
+				Pattern pattern = Pattern.compile("xpath(.*)text(.*)='([\\sa-zA-Z>]+)']");
+				
+				Matcher matcher = pattern.matcher(xpathValue);
+				
+				while(matcher.find()){
+					
+					String splittedXpath = matcher.group(3);					
+					
+					System.out.println("splittedXpath3 :"+splittedXpath);
+					
+					xpathsplittext = splittedXpath.split(">>");
+					
+					resourceFileName = xpathsplittext[0] + excel.RetrieveAutomationKeyFromExcel("Extension_Language", "Resx_Extension", languageName);
+					
+					filePathLocation=property.getProperty("resourcesFilePath_GBR");
+					String resourceFileToGetValue=filePathLocation+"\\"+resourceFileName;
+					log.info("File path is "+resourceFileToGetValue);
+					File file=new File("//\\"+resourceFileToGetValue);
+
+					String commonAttribute=property.getProperty("attributeCommonValue");
+					String fullAttributewithName=commonAttribute+"[@name='"+ xpathsplittext[1] +"']/value";
+					log.info("Attribute to Retrieve Node value is : "+fullAttributewithName);
+
+					DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+					DocumentBuilder builder=factory.newDocumentBuilder();
+					Document document=builder.parse(file);
+					document.getDocumentElement().normalize();
+					XPath xpath=XPathFactory.newInstance().newXPath();
+					nodeList=(NodeList)xpath.compile(fullAttributewithName).evaluate(document,XPathConstants.NODESET);
+
+					Resxfilevalue = nodeList.item(0).getTextContent();
+					
+					if(Resxfilevalue.contains("'")){
+						SplitResxfilevalue = Resxfilevalue.split("'");
+						
+						for(int m = 1; m <= SplitResxfilevalue.length -1; m++){
+							if(m < 2){
+								finalresxfilevalue = SplitResxfilevalue[m-1] +"',\"'\",'"+ SplitResxfilevalue[m];
+							} else{
+								finalresxfilevalue = finalresxfilevalue + "',\"'\",'"+ SplitResxfilevalue[m];
+							}
+						}
+						
+						xpathValue =  xpathValue.replaceAll("'"+ splittedXpath +"'", "concat('"+ finalresxfilevalue +"')");
+					} else{
+						xpathValue = xpathValue.replaceAll(splittedXpath, Resxfilevalue);
+					}
+					
+				
+					} 
+				
+			} else if(xpathValue.contains("contains(text()")){
+				
+				System.out.println("contains(text()");
+				
+				Pattern pattern = Pattern.compile("(.*)contains(.*)text(.*),'(.*)'");
+				
+				Matcher matcher = pattern.matcher(xpathValue);
+				
+				while(matcher.find()){
+					
+					String splittedXpath = matcher.group(4);
+					
+					System.out.println("splittedXpath :"+splittedXpath);
+					
+					xpathsplittext = splittedXpath.split(">>");
+					
+					resourceFileName = xpathsplittext[0] + excel.RetrieveAutomationKeyFromExcel("Extension_Language", "Resx_Extension", languageName);
+					
+					filePathLocation=property.getProperty("resourcesFilePath_GBR");
+					String resourceFileToGetValue=filePathLocation+"\\"+resourceFileName;
+					log.info("File path is "+resourceFileToGetValue);
+					File file=new File("//\\"+resourceFileToGetValue);
+
+					String commonAttribute=property.getProperty("attributeCommonValue");
+					String fullAttributewithName=commonAttribute+"[@name='"+ xpathsplittext[1] +"']/value";
+					log.info("Attribute to Retrieve Node value is : "+fullAttributewithName);
+
+					DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+					DocumentBuilder builder=factory.newDocumentBuilder();
+					Document document=builder.parse(file);
+					document.getDocumentElement().normalize();
+					XPath xpath=XPathFactory.newInstance().newXPath();
+					nodeList=(NodeList)xpath.compile(fullAttributewithName).evaluate(document,XPathConstants.NODESET);
+
+					Resxfilevalue = nodeList.item(0).getTextContent();
+					
+					if(Resxfilevalue.contains("'")){
+						SplitResxfilevalue = Resxfilevalue.split("'");
+						
+						for(int m = 1; m <= SplitResxfilevalue.length -1; m++){
+							if(m < 2){
+								finalresxfilevalue = SplitResxfilevalue[m-1] +"',\"'\",'"+ SplitResxfilevalue[m];
+							} else{
+								finalresxfilevalue = finalresxfilevalue + "',\"'\",'"+ SplitResxfilevalue[m];
+							}
+						}
+						
+						xpathValue =  xpathValue.replaceAll("'"+ splittedXpath +"'", "concat('"+ finalresxfilevalue +"')");
+					} else{
+						xpathValue = xpathValue.replaceAll(splittedXpath, Resxfilevalue);
+					}
+					
+		
+					}
+				
+				}
+		
+		return xpathValue;
 
 		}catch(Exception e){
 			log.info("Error occurred in POM classes :"+e);
