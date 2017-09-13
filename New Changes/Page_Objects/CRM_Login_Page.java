2430f1b6c7f5b7da92b7848f70a28ca67f3a71af
ManiR02 <mani.cse02@gmail.com>
2017-09-13 17:52:59 +0530

    New changes added
    commit

--- New Changes/Page_Objects/CRM_Login_Page.java
+++ New Changes/Page_Objects/CRM_Login_Page.java
@@ -2,6 +2,7 @@ package Page_Objects;
 
 import java.util.Hashtable;
 
+
 public class CRM_Login_Page{
 
 	public synchronized String Login_Page(String locator){
@@ -10,6 +11,9 @@ public class CRM_Login_Page{
 			Hashtable<String, String> hs = new Hashtable<String, String>();		
 			hs.put("txtbox_UserName", "id#UserName");
 			hs.put("txtbox_Password", "id#Password");
+			hs.put("btn_LogIn", "id#Login");
+			hs.put("Globe_Icon", "xpath#//span[@class='dropdown-toggle glyphicon glyphicon-globe']");
+			hs.put("Language_List", "xpath#//ul[@class='dropdown-menu dropdown-menu-right']/li/a");
 			hs.put("btn_LogIn", "id#Login");
 			return hs.get(locator);
 		}catch(Exception e){
